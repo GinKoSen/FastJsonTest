22e3cd1130028ce9d5131627e5a8b58fe10d8943
ginko.wang <yingosen@gmail.com>
2016-05-26 17:46:33 +0800

    commit changes

--- src/enums/TestEnmu.java
+++ src/enums/TestEnmu.java
@@ -9,10 +9,13 @@ import com.alibaba.fastjson.serializer.SerializeConfig;
  * @date 2016年5月26日 下午3:32:02
  */
 public class TestEnmu {
+	static {
+		SerializeConfig.getGlobalInstance().put(EnumA.class, EnumSerializer.instance);
+	}
+
 	public static void main(String[] args) {
 		BA ba = new BA();
 		ba.color = EnumA.A;
-		SerializeConfig.getGlobalInstance().put(EnumA.class, EnumSerializer.instance);
 		System.err.println(JSON.toJSONString(ba));
 	}
 }
